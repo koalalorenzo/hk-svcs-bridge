078d27faedb6475f39fb7f36138582a77d5d4796
Lorenzo Setale <lorenzo@setale.me>
2023-03-07 21:20:11 +0100

    adds commands for On and Off
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

--- services.go
+++ services.go
@@ -3,34 +3,59 @@ package main
 import (
 	"fmt"
 	"log"
+	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/brutella/hap/accessory"
 )
 
 type SystemDService struct {
-	Name        string            `yaml:"name"`
-	ServiceName string            `yaml:"service_name"`
-	OnCommand   string            `yaml:"oncmd"`
-	OffCommand  string            `yaml:"offcmd"`
-	Accessory   *accessory.Switch `yaml:"-"`
+	// Name visible in the Homekit app
+	Name string `yaml:"name"`
+	// ServiceName is the SystemD Service name
+	ServiceName string `yaml:"service_name"`
+	// OnCommand is useful if we want to customize what command to run when
+	// the trigger is ON
+	OnCommand string `yaml:"oncmd"`
+	// OffCommand is like OnCommand but runs when the trigger is set to Off
+	OffCommand string `yaml:"offcmd"`
+	// SystemDPeriodicCheck if true will periodically check the status of th
+	// SystemD ServiceName.
+	SystemDPeriodicCheck bool `yaml:"systemd_check"`
+
+	// Accessory is t he HAP accessory
+	Accessory *accessory.Switch `yaml:"-"`
+	// Updating is used to prevent the CheckStatus to interfere with the cmds
+	IsUpdating bool `yaml:"-"`
 }
 
-func (s *SystemDService) Off() {
-	cmd := fmt.Sprintf("services %s stop", s.ServiceName)
-	if s.OffCommand != "" {
-		cmd = s.OffCommand
-	}
+var systemctlBinaryPath string
 
-	log.Printf("Running %s", cmd)
+func init() {
+	systemctlBinaryPath, _ = exec.LookPath("systemctl")
 }
 
-func (s *SystemDService) On() {
-	cmd := fmt.Sprintf("services %s start", s.ServiceName)
-	if s.OnCommand != "" {
-		cmd = s.OnCommand
+func (s *SystemDService) runCmd(cmd string) {
+	if s.IsUpdating {
+		return
 	}
+	s.IsUpdating = true
+	defer func() { s.IsUpdating = false }()
 
-	log.Printf("Running %s", cmd)
+	log.Printf("Running %s", s.OffCommand)
+	run := exec.Command(strings.Split(s.OffCommand, " ")...)
+	out, err := run.CombinedOutput()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			exitCode := exitError.ExitCode()
+			log.Printf("Error encountered %v:\n %v", exitCode, out)
+		}
+	}
+	s.Accessory.Switch.On.SetValue(false)
+
+	// Prevent Updating during the to avoid overlapping
+	time.Sleep(time.Duration(500) * time.Millisecond)
 }
 
 func (s *SystemDService) Init() {
