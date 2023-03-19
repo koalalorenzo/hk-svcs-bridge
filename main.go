86ab9f265ed03130d48c5e048962ec5a175d65ab
Lorenzo Setale <lorenzo@setale.me>
2023-03-19 12:57:21 +0100

    fixes unbuffered channel
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 
 	// Setup a listener for interrupts and SIGTERM signals
 	// to stop the server.
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	signal.Notify(c, syscall.SIGTERM)
 
